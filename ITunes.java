93a58698577ca907ca84fdabbb82d3d948e98126
JacobxChoi <74112837+JacobxChoi@users.noreply.github.com>
2021-04-09 16:26:09 -0400

    Add files via upload

---
+++ ITunes.java
@@ -0,0 +1,103 @@
+public class ITunes{
+
+  public static void main(String[] args){
+
+    // Sample testing code ...
+    Playlist pl = new Playlist();
+
+    //test displayBackwards()
+    //pl.displayPlaylistBackward();
+
+    //test deleteEveryMthEpisode()
+    //pl.deleteEveryMthEpisode(3);
+
+    // pl.addLast("PlanetMoney",26.0);
+    // pl.addLast("HowIBuiltThis",10);
+    // pl.addLast("EzraKleinShow",65.0);
+    // pl.addLast("RadioLab",25.5);
+    // pl.deleteEveryMthEpisode(-1);
+
+    // pl.addLast("PlanetMoney",26.0);
+    // pl.addLast("HowIBuiltThis",10);
+    // pl.addLast("EzraKleinShow",65.0);
+    // pl.addLast("RadioLab",25.5);
+    // pl.addLast("MakeMeSmart",24.5);
+    // pl.addLast("Worldly",55);
+    // pl.addLast("Explained",23.0);
+    // pl.addLast("Invisibilia",33.5);
+
+    // pl.displayPlaylistForward();
+    // pl.displayPlaylistBackward();
+
+    // System.out.println(pl.deleteEveryMthEpisode(3));
+
+    //test addFirst()
+    // pl.addFirst("PlanetMoney",26.0);
+    // pl.addFirst("HowIBuiltThis",10);
+    // pl.displayPlaylistForward();
+    // pl.displayPlaylistBackward();
+    
+    //test add()
+    // pl.addLast("PlanetMoney",26.0);
+    // pl.addLast("HowIBuiltThis",10);
+    // pl.addLast("EzraKleinShow",65.0);
+    // pl.addLast("RadioLab",25.5);
+
+    //all 3 cases
+    //pl.add("Worldly",55, 4);
+    //pl.add("Worldly",55, 3);
+    //pl.add("Worldly",55, 0);
+    // pl.displayPlaylistForward();
+    // pl.displayPlaylistBackward();
+
+    //should have error
+    //pl.add("Worldly",55, -1);
+    //pl.add("Worldly",55, 5);
+    // pl.displayPlaylistForward();
+    // pl.displayPlaylistBackward();
+
+    //test deleteFirst()
+    //pl.deleteFirst();
+    // pl.addLast("PlanetMoney",26.0);
+    // pl.addLast("HowIBuiltThis",10);
+    // pl.addLast("EzraKleinShow",65.0);
+    // pl.addLast("RadioLab",25.5);
+    // pl.deleteFirst();
+    // pl.displayPlaylistForward();
+    // pl.displayPlaylistBackward();
+    
+    // pl.addLast("PlanetMoney",26.0);
+    // System.out.println(pl.deleteFirst());
+    // pl.displayPlaylistForward();
+
+    //doesn't work temporarily
+    //pl.displayPlaylistBackward();
+
+    //test deleteLast()
+    // pl.addLast("PlanetMoney",26.0);
+    // System.out.println(pl.deleteLast());
+    // pl.displayPlaylistForward();
+
+    // pl.addLast("PlanetMoney",26.0);
+    // pl.addLast("HowIBuiltThis",10);
+    // pl.addLast("EzraKleinShow",65.0);
+    // System.out.println(pl.deleteLast());
+    // pl.displayPlaylistForward();
+    // pl.displayPlaylistBackward();
+
+    //test deleteEpisode()
+    //pl.deleteEpisode("PlanetMoney");
+
+    // pl.addLast("PlanetMoney",26.0);
+    // pl.addLast("HowIBuiltThis",10);
+    // pl.addLast("EzraKleinShow",65.0);
+    // pl.addLast("RadioLab",25.5);
+    // pl.addLast("MakeMeSmart",24.5);
+    //System.out.println(pl.deleteEpisode("PlanetMoney"));
+    //System.out.println(pl.deleteEpisode("HowIBuiltThis"));
+    //System.out.println(pl.deleteEpisode("MakeMeSmart"));
+    //System.out.println(pl.deleteEpisode(""));
+    // pl.displayPlaylistForward();
+
+  }
+}
